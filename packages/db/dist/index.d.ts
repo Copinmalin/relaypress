e2e0fa0d0e25b5cd7fbc3c553afdec1fb0033142
Copinmalin <cyrille@coppere.net>
2026-06-06 16:22:29 +0200

    fix(ci): type db declaration methods used by services

--- packages/db/dist/index.d.ts
+++ packages/db/dist/index.d.ts
@@ -1,17 +1,39 @@
-export declare function createPgPool(connectionString?: string): unknown;
-
-export declare function createDb(connectionString?: string): {
-  db: unknown;
-  pool: unknown;
+type InsertResult = {
+  returning<T extends Record<string, unknown>>(selection: T): Promise<Array<{ [K in keyof T]: string }>>;
 };
 
-export declare function migrate(pool: unknown): Promise<void>;
+type InsertValues = {
+  onConflictDoNothing(): InsertResult;
+};
 
-export type RelayPressDb = unknown;
+type DbClient = {
+  insert(table: unknown): {
+    values(value: Record<string, unknown>): InsertValues;
+  };
+};
 
-export declare const nostrEvents: unknown;
-export declare const sourceItems: unknown;
-export declare const editorialSignals: unknown;
-export declare const publicationJobs: unknown;
-export declare const publicationJobRuns: unknown;
-export declare const publisherAccounts: unknown;
+type PoolLike = {
+  query(query: string, values?: unknown[]): Promise<unknown>;
+};
+
+type TableWithId = {
+  id: unknown;
+};
+
+export declare function createPgPool(connectionString?: string): PoolLike;
+
+export declare function createDb(connectionString?: string): {
+  db: DbClient;
+  pool: PoolLike;
+};
+
+export declare function migrate(pool: PoolLike): Promise<void>;
+
+export type RelayPressDb = DbClient;
+
+export declare const nostrEvents: TableWithId;
+export declare const sourceItems: TableWithId;
+export declare const editorialSignals: TableWithId;
+export declare const publicationJobs: TableWithId;
+export declare const publicationJobRuns: TableWithId;
+export declare const publisherAccounts: TableWithId;
